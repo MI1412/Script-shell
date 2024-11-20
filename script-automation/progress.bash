1dd2e2847b8f11714f12e3d87b9c7f6d38c13506
MI1412 <imronm1309@gmail.com>
2024-11-20 15:17:02 +0700

    menambahkan script baru

---
+++ script-automation/progress.bash
@@ -0,0 +1,43 @@
+# instalasi
+echo ""
+echo "--- instalasi ---"
+echo "coba pasang fontawesome"
+echo ""
+
+# target bulan ini
+echo "--- target bulan ini ---"
+echo "november: coba buat class yang saling berelasi"
+echo "next 2025: manipulasi data"
+echo "2025 Paham: belajar standart library, class oop built in tipe, built in exception"
+echo "01 januari: tahu lokasi objek di memori python dan belajar callback"
+echo "desember: tahu objek python"
+echo ""
+
+echo "--- saran ---"
+echo "sampai mengoleksi catatan (add_notes) exceptions"
+echo "belajar bahasa inggris"
+echo ""
+
+# build tools
+echo "--- buid tools ---"
+echo "buat tools untuk automasi git selengkapnya di my-note/automasi_git.txt"
+echo "kerjakan dihari minggu: pasang peps di lokal dan menjalankannya"
+echo "desember : ngulik home server"
+echo ""
+
+# linux && bsd
+echo "--- linux bsd ---"
+echo "belajar cmd gnu linux lengkap: fileNameManipulation"
+echo ""
+
+# backup
+#/d/File Gambar, video, Dokumen, DLL
+# backup D:\foto dan video
+# rapikan : D:\redmi4x, D:\software tools
+
+# project
+echo "--- project ---"
+echo "tenggat januari 2025: buat log otomatis jika memasukkan file ke komputer"
+echo "tenggat februari 2025: buat kode editor di hp pakai python"
+echo "tenggat maret 2025: buat aplikasi notifikasi hp dan laptop / pengingat dengan python"
+echo ""
